c32b82fe61736408d8ae6ff43ca6de2685ffe302
clippinglee <clippinglp@qq.com>
2025-01-01 18:31:39 +0800

    version 1.0.1 add channel and point check

--- L298N.c
+++ L298N.c
@@ -1,12 +1,30 @@
+/*
+*     _______ .__   __.   _______  __  .__   __.  _______  _______ .______         .______    __  .__   __.   _______
+*    |   ____||  \ |  |  /  _____||  | |  \ |  | |   ____||   ____||   _  \        |   _  \  |  | |  \ |  |  /  _____|
+*    |  |__   |   \|  | |  |  __  |  | |   \|  | |  |__   |  |__   |  |_)  |       |  |_)  | |  | |   \|  | |  |  __  
+*    |   __|  |  . `  | |  | |_ | |  | |  . `  | |   __|  |   __|  |      /        |   ___/  |  | |  . `  | |  | |_ |
+*    |  |____ |  |\   | |  |__| | |  | |  |\   | |  |____ |  |____ |  |\  \----.   |  |      |  | |  |\   | |  |__| |
+*    |_______||__| \__|  \______| |__| |__| \__| |_______||_______|| _| `._____|   | _|      |__| |__| \__|  \______|
+*                                                                                                                    
+*/
 
 #ifdef __cplusplus
 extern "C"{
 #endif
+#include "stddef.h"
 #include "L298N.h"
 
 Std_Return L298N_MotorControl(int channel,unsigned char rotation )
 {
+    if(channel >= L298N_MOTOR_CHANNEL_MAX)
+    {
+        return 1;
+    }
     L298N_PinType_t* pinControl = &L298N_ChannelCfg[channel];
+    if(pinControl->pinWirteFunc == NULL)
+    {
+        return 1;
+    }
     if(rotation == MOTOR_FORWARD)
     {
         pinControl->pinWirteFunc(pinControl->pinNumA,1);
@@ -28,8 +46,17 @@ Std_Return L298N_MotorControl(int channel,unsigned char rotation )
 
 Std_Return L298N_MotorSpeed(int channel,float speed)
 {
+    if(channel >= L298N_MOTOR_CHANNEL_MAX)
+    {
+        return 1;
+    }
     L298N_PinType_t* pinControl = &L298N_ChannelCfg[channel];
+    if(pinControl->pwmWriteFunc == NULL)
+    {
+        return 1;
+    }
     pinControl->pwmWriteFunc(pinControl->pwmNNum,speed);
+    return 0;
 }
 
 
--- L298N.h
+++ L298N.h
@@ -1,3 +1,13 @@
+/*
+*     _______ .__   __.   _______  __  .__   __.  _______  _______ .______         .______    __  .__   __.   _______
+*    |   ____||  \ |  |  /  _____||  | |  \ |  | |   ____||   ____||   _  \        |   _  \  |  | |  \ |  |  /  _____|
+*    |  |__   |   \|  | |  |  __  |  | |   \|  | |  |__   |  |__   |  |_)  |       |  |_)  | |  | |   \|  | |  |  __  
+*    |   __|  |  . `  | |  | |_ | |  | |  . `  | |   __|  |   __|  |      /        |   ___/  |  | |  . `  | |  | |_ |
+*    |  |____ |  |\   | |  |__| | |  | |  |\   | |  |____ |  |____ |  |\  \----.   |  |      |  | |  |\   | |  |__| |
+*    |_______||__| \__|  \______| |__| |__| \__| |_______||_______|| _| `._____|   | _|      |__| |__| \__|  \______|
+*                                                                                                                    
+*/
+
 #ifndef __L298N__H_
 #define __L298N__H_
 
@@ -8,10 +18,13 @@ extern "C"{
 #define MOTOR_FORWARD 2U
 #define MOTOR_REVERSE 1U
 #define MOTOR_STOP    0U
+
+#define L298N_MOTOR_CHANNEL_MAX
+
 typedef unsigned char Std_Return;
 
 typedef void (*pinWirteFunc_t)(uint8_t,uint8_t);
-typedef void (*pwmWirteFunc_t)(uint8_t,uint8_t);
+typedef void (*pwmWirteFunc_t)(uint8_t,float);
 typedef struct L298N_PinType{
     int pinNumA;
     int pinNumB;
@@ -19,9 +32,10 @@ typedef struct L298N_PinType{
     int pwmNNum;
     pwmWirteFunc_t pwmWriteFunc;
 }L298N_PinType_t;
-extern L298N_PinType_t L298N_ChannelCfg[];
+extern L298N_PinType_t L298N_ChannelCfg[L298N_MOTOR_CHANNEL_MAX];
 Std_Return L298N_MotorControl(int channel,unsigned char rotation );
 Std_Return L298N_MotorSpeed(int channel,float speed);
+
 #ifdef __cplusplus
 }
 #endif /*__cplusplus*/
